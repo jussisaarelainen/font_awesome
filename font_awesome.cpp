9b99321fdf635036a33c5e491cbccb8eb8b24b15
Jussi Saarelainen <jussi3001@yahoo.com>
2019-06-02 17:25:46 +0300

    Initial commit

---
+++ font_awesome.cpp
@@ -0,0 +1,11 @@
+#ifdef JUCE_AWESOME_FONT_H_INCLUDED
+ /* When you add this cpp file to your project, you mustn't include it in a file where you've
+    already included any other headers - just put it inside a file on its own, possibly with your config
+    flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
+    header files that the compiler may be using.
+ */
+ #error "Incorrect use of JUCE cpp file"
+#endif
+
+#include "font_awesome.h"
+#include "source/font_awesome.cpp"
---
+++ font_awesome.h
@@ -0,0 +1,30 @@
+/*******************************************************************************
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               font_awesome
+  vendor:           Jussi Saarelainen
+  version:          0.1.0
+  name:             Font Awesome
+  description:      Font Awesome as Juce sub-module
+  website:          http://www.fontawesome.com
+  license:
+
+  dependencies:     juce_gui_basics
+  OSXFrameworks:
+  iOSFrameworks:
+  linuxPackages:
+  mingwLibs:
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+
+#pragma once
+#define JUCE_AWESOME_FONT_H_INCLUDED
+
+#include <juce_gui_basics/juce_gui_basics.h>
+
+#include "source/font_awesome.h"
+
